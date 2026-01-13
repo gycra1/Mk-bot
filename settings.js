2bed57010f60176f0c26af4de3a355ad25fbc71f
gycra1 <166731285+gycra1@users.noreply.github.com>
2026-01-13 16:21:57 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,15 +20,15 @@ const chalk = require('chalk')
 
 //———————[ Owner Config ]——————————//
 
-global.ownernumber = '263714757857' // Change to your number
-global.ownername = 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ' // add ur name
+global.ownernumber = '255748028271' // Change to your number
+global.ownername = 'GYCRA JORDAN' // add ur name
 global.botMode ='public' // change to public or self
 
 //————————[ Bot Config ]—————————//
 
 global.SESSION_ID = ""; // add ur session id starts with starcore~
-global.namabot = "ᴍᴋ-ʙᴏᴛ"
-global.nomorbot = '263714757857' // Change to yourbot number
+global.namabot = "GYCRA-BOT"
+global.nomorbot = '255748028271' // Change to yourbot number
 global.pair = "MRMALVIN"
 global.version = '1.0.1'
 global.autojoingc = false
